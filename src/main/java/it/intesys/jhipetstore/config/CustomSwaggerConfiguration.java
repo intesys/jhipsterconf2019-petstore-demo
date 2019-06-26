d7db95a64744ee6d4dbe6e1ad15d6e419be40f8d
Enrico Costanzi <enrico.costanzi@gmail.com>
2019-06-26 09:33:17 +0200

    add security rules to springfox dockets

--- src/main/java/it/intesys/jhipetstore/config/CustomSwaggerConfiguration.java
+++ src/main/java/it/intesys/jhipetstore/config/CustomSwaggerConfiguration.java
@@ -10,12 +10,18 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.zalando.problem.Problem;
 import org.zalando.problem.StatusType;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -54,6 +60,8 @@ public class CustomSwaggerConfiguration {
             .directModelSubstitute(StatusType.class, Integer.class)
             .genericModelSubstitutes(ResponseEntity.class)
             .additionalModels(typeResolver.resolve(Problem.class))
+            .securitySchemes(Collections.singletonList(jwtHeaderKey()))
+            .securityContexts(securityContexts(bearerAuth(),"/api/account", "/api/account/change-password"))
             .select()
             .paths(Predicates.or(
                 regex("/api/account.*"), regex("/api/authenticate.*"),
@@ -86,10 +94,46 @@ public class CustomSwaggerConfiguration {
             .directModelSubstitute(StatusType.class, Integer.class)
             .genericModelSubstitutes(ResponseEntity.class)
             .additionalModels(typeResolver.resolve(Problem.class))
+            .securitySchemes(Collections.singletonList(jwtHeaderKey()))
             .select()
             .paths(regex("/api-first.*"))
             .build();
         return docket;
     }
+
+    /**
+     * Bearer security scheme of the entire json spec file
+     * @return a Security Scheme
+     */
+    private static SecurityScheme jwtHeaderKey() {
+        return new ApiKey("jwt", "Authorization", "header");
+    }
+
+    /**
+     * Enables the given list of {@link SecurityReference} for all the paths in the paths list
+     * @param securityReferences
+     * @param paths
+     * @return
+     */
+    private static List<SecurityContext> securityContexts(List<SecurityReference> securityReferences, String... paths){
+        return Arrays.stream(paths)
+            .map(pathItem ->
+                SecurityContext.builder()
+                    .securityReferences(securityReferences)
+                    .forPaths(PathSelectors.regex(pathItem))
+                    .build())
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * security:
+     *  - Bearer: []
+     *
+     * @return a singleton list with one Bearer security reference
+     */
+    private static List<SecurityReference> bearerAuth() {
+        return Collections.singletonList(new SecurityReference("jwt", new AuthorizationScope[0]));
+    }
+
 
 }
